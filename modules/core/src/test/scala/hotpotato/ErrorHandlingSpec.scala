41981c29ff952fee1d8342132b33fa18ab157e4e
Jacob Wang <jatcwang@gmail.com>
2019-11-28 23:02:10 +0000

    move tests

--- modules/core/src/test/scala/hotpotato/ErrorHandlingSpec.scala
+++ modules/core/src/test/scala/hotpotato/ErrorHandlingSpec.scala
@@ -7,6 +7,7 @@ import shapeless._
 import shapeless.syntax.inject._
 import cats.implicits._
 import hotpotato.util.AssertType
+import zio.Exit
 
 class ErrorHandlingSpec extends AnyWordSpec with Matchers {
   import ErrorTrans._
@@ -292,4 +293,35 @@ class ErrorHandlingSpec extends AnyWordSpec with Matchers {
     }
   }
 
+  "unifyError" should {
+    "unify the error into one" in {
+      import hotpotato.PureExamples._
+      inside(b_E12_1.unifyError) {
+        case Left(E1()) => succeed
+      }
+    }
+  }
+
+  "dieIf" should {
+    "dieIf throws the case as the exception (one type param)" in {
+      import ZioExamples._
+
+      (unsafeRun(b_E1234_1.dieIf[E1]): Exit[E234, String]) shouldBe Exit.die(e1)
+
+      (unsafeRun(b_E1234_2.dieIf[E1]): Exit[E234, String]) shouldBe Exit.fail(e2.inject[E234])
+      (unsafeRun(b_E1234_3.dieIf[E1]): Exit[E234, String]) shouldBe Exit.fail(e3.inject[E234])
+    }
+
+    "throws all selected types when selected" in {
+      import ZioExamples._
+
+      (unsafeRun(b_E1234_1.dieIf[E1, E3]): Exit[E24, String]) shouldBe Exit.die(e1)
+
+      (unsafeRun(b_E1234_2.dieIf[E1, E3]): Exit[E24, String]) shouldBe Exit.fail(e2.inject[E24])
+
+      (unsafeRun(b_E1234_3.dieIf[E1, E3]): Exit[E24, String]) shouldBe Exit.die(e3)
+
+      (unsafeRun(b_E1234_4.dieIf[E1, E3]): Exit[E24, String]) shouldBe Exit.fail(e4.inject[E24])
+    }
+  }
 }
